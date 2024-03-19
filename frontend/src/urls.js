59692a83bc9eb2e0eb90e05c939c6565b4a29ffe
Akshay Bhasin <107040611+Akshay9704@users.noreply.github.com>
2024-03-19 15:47:34 +0530

    Update base URL in urls.js

--- frontend/src/urls.js
+++ frontend/src/urls.js
@@ -1,3 +1,3 @@
-export const baseUrl = 'https://webalar-backend.vercel.app';
+export const baseUrl = 'https://webalar-project-1.onrender.com';
 
 //change the base url from urls.js file to http://localhost:8000 and don't use "/" at the end of the url.
